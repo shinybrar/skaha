7ed2961b616bec1f745c146710c78a11f197cd54
Shiny Brar <shiny.brar@nrc-cnrc.gc.ca>
2025-03-06 17:12:42 -0800

    test(async-session): added tests to create and destroy async sessions

--- tests/test_async_session.py
+++ tests/test_async_session.py
@@ -1,3 +1,4 @@
+from typing import List
 from uuid import uuid4
 
 import pytest
@@ -55,3 +56,37 @@ async def test_create_session_invalid(async_session: AsyncSession, name: str):
             kind="invalid",  # type: ignore
             image="jupyter/base-notebook",
         )
+
+
+@pytest.mark.asyncio
+@pytest.mark.order(1)
+async def test_create_session(async_session: AsyncSession, name: str):
+    """Test creating a session."""
+    identity: List[str] = await async_session.create(
+        name=name,
+        kind="headless",
+        cores=1,
+        ram=1,
+        image="images.canfar.net/skaha/terminal:1.1.2",
+        cmd="env",
+        replicas=1,
+        env={"TEST": "test"},
+    )
+    assert len(identity) == 1
+    assert identity[0] != ""
+    pytest.IDENTITY = identity  # type: ignore
+
+
+@pytest.mark.asyncio
+@pytest.mark.order(2)
+async def test_delete_session(async_session: AsyncSession, name: str):
+    """Test deleting a session."""
+    # Delete the session
+    done = False
+    while not done:
+        info = await async_session.info(ids=pytest.IDENTITY)  # type: ignore
+        for status in info:
+            if status["status"] == "Succeeded":
+                done = True
+    deletion = await async_session.destroy_with(prefix=name)  # type: ignore
+    assert deletion == {pytest.IDENTITY[0]: True}  # type: ignore
